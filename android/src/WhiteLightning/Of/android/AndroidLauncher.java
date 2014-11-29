0de9347daf1b316a0814d00e956a318cd018f454
mrlogan <sebastian.korczyk@gmail.com>
2014-11-29 17:24:04 +0100

    baseline

---
+++ android/src/WhiteLightning/Of/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package WhiteLightning.Of.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import WhiteLightning.Of.Oil;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Oil(), config);
+	}
+}
---
+++ desktop/src/WhiteLightning/Of/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package WhiteLightning.Of.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import WhiteLightning.Of.Oil;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Oil(), config);
+	}
+}
